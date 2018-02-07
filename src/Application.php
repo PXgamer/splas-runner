c9b4ca7a9cdd6a30f0c502baf95424295b122f6f
pxgamer <owzie123@gmail.com>
2018-02-07 10:25:57 +0000

    Update to allow better versioning using tags
    Changed to allow better tag versioning from Git. By default, it will
    show the version as 'source' unless it's been compiled as a Phar.

---
+++ src/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace pxgamer\SplasRunner;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+    const NAME = 'Splas Runner';
+    const VERSION = '@git-version@';
+
+    public function __construct($name = null, $version = null)
+    {
+        parent::__construct($name ?: static::NAME, $version ?: (static::VERSION === '@' . 'git-version@' ? 'source' : static::VERSION));
+    }
+
+    protected function getDefaultCommands()
+    {
+        $commands = parent::getDefaultCommands();
+
+        $commands[] = new RunCommand();
+
+        return $commands;
+    }
+}
