b664ee04b0039b19db36a40e9e623176782a8729
pxgamer <owzie123@gmail.com>
2018-02-08 10:48:29 +0000

    Add missing PhpDoc comments

--- src/Application.php
+++ src/Application.php
@@ -4,11 +4,20 @@ namespace pxgamer\SplasRunner;
 
 use Symfony\Component\Console\Application as BaseApplication;
 
+/**
+ * Class Application
+ */
 class Application extends BaseApplication
 {
     const NAME = 'Splas Runner';
     const VERSION = '@git-version@';
 
+    /**
+     * Application constructor.
+     *
+     * @param null $name
+     * @param null $version
+     */
     public function __construct($name = null, $version = null)
     {
         parent::__construct(
@@ -17,6 +26,9 @@ class Application extends BaseApplication
         );
     }
 
+    /**
+     * @return array|\Symfony\Component\Console\Command\Command[]
+     */
     protected function getDefaultCommands()
     {
         $commands = parent::getDefaultCommands();
